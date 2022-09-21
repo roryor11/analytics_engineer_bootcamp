b32324c4f076045eb473e71e6c25d94abfba76c6
roryor11 <rory.oriordan@hotmail.com>
2022-09-21 19:28:13 +0100

    Added ingestion tsp

--- ae_bootcamp/models/staging/stg_employee_privileges.sql
+++ ae_bootcamp/models/staging/stg_employee_privileges.sql
@@ -2,4 +2,6 @@ with source as (
     select * from {{ source('northwind', 'employee_privileges')}}
 )
 
-select * from source
+select *
+        ,current_timestamp() as ingestion_timestamp
+from source
